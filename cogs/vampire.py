f5849a88a4f577bf05a57fa5dec16d9f50255304
jca <joe@accardi.xyz>
2024-12-13 18:55:29 -0500

    added vampire flow 5

--- cogs/vampire.py
+++ cogs/vampire.py
@@ -8,16 +8,25 @@ class Vampire(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='vampire', help='Pulls a random line from a text file and repeats it back.')
-    async def vampire(self, ctx):
+    @commands.command(name='vampire', help='vampire flow')
+    async def vampire(self, ctx, word: str = None):
         try:
             with open('/home/jca/dev/python/sharebro/cogs/vampire_flow.txt', 'r') as file:
                 lines = file.readlines()
-                if lines:
-                    quote = random.choice(lines).strip()
+
+                # Filter the lines if a word is provided
+                if word:
+                    filtered_lines = [line for line in lines if word.lower() in line.lower()]
+                else:
+                    filtered_lines = lines
+
+                # Choose a random line if there are matching results
+                if filtered_lines:
+                    quote = random.choice(filtered_lines).strip()
                     await ctx.send(quote)
                 else:
-                    await ctx.send('The file is empty. Please add some quotes.')
+                    await ctx.send(f'No quotes found containing the word "{word}".')
+
         except FileNotFoundError:
             await ctx.send('The quotes file could not be found. Make sure "vampire_flow.txt" is in the correct directory.')
 
